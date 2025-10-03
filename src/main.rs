119cb882be041cef6347e07cbd27eab233bd64c7
Tantatorn <timefear007@gmail.com>
2025-10-03 21:12:16 +0700

    feat: enhance main application structure with systems and components

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, initialize)
+        .add_systems(Update, hello_world)
+        .run();
+}
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+fn initialize(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((Position { x: 0.0, y: 0.0 },));
+}
+
+fn hello_world() {
+    println!("hello world!");
 }
